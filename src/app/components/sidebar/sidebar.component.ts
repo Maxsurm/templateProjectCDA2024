fc1a20ff333dfdbffd442d2a8660bcaa7abb8c1f
Yanis Adekalom <yanis971.60@gmail.com>
2024-01-22 12:30:19 +0100

    Feat : SideBar

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  href : string = "#"
+  src: string = "pic07.jpg"
+  alt?: string;
+  description: string = "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam."
+
+  miniPostClass: MiniPostClass = new MiniPostClass("#", "pic08.jpg", this.description)
+  miniPostDefault = new MiniPostClassDefault();
+  miniPost: MiniPost = {
+    description: this.description,
+    href: "#",
+    src: "pic09.jpg",
+  }
+  constructor() {
+    this.miniPostClass.alt = "Alternatif";
+    this.miniPostDefault.description = "Try Me"
+  }
+
+}
+
+class MiniPostClass {
+  href: string;
+  src: string;
+  alt?: string;
+  description: string;
+
+  constructor(href: string, src: string, description: string, alt?: string) {
+    this.href = href;
+    this.src = src;
+    this.description = description;
+    this.alt = alt
+  }
+}
+
+class MiniPostClassDefault {
+  href: string = "#";
+  src: string = "pic01.jpg";
+  alt?: string;
+  description: string = "";
+}
+
+interface MiniPost {
+  href: string;
+  src: string;
+  alt?: string;
+  description: string;
+}
