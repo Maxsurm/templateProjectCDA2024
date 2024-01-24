f77a2a7078e587741f340f1ed3f37facb9ee8a0c
Yanis Adekalom <yanis971.60@gmail.com>
2024-01-24 15:56:44 +0100

    Feat : Reactive Form Tools

--- src/app/components/form-group/form-group.component.ts
+++ src/app/components/form-group/form-group.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { getFormControl, hasControlError, isControlInvalid } from 'src/app/tools/reactive-form-tools';
 
 @Component({
   selector: 'app-form-group',
@@ -7,8 +9,42 @@ import { Component } from '@angular/core';
 })
 export class FormGroupComponent {
 
-  onSubmit() {
-    
-}
+  form: FormGroup = new FormGroup({
+    id: new FormControl(0),
+    imageSrc: new FormControl(""),
+    imageAlt: new FormControl("Image Alt", [Validators.required]),
+    titre: new FormControl("", [Validators.required, Validators.minLength(10), Validators.maxLength(50)]),
+    description: new FormControl("", [Validators.required]),
+    postLink: new FormControl("#", [Validators.required])
+  });
 
+  onSubmit() {
+    if(this.form.valid) {
+      console.log("Article : ", this.form.value)
+    }
+  }
+
+  get jsonValue() {
+    return JSON.stringify(this.form.value)
+  }
+
+  get titre() {
+    return this.form.get('titre')
+  }
+
+  get titreInvalid() {
+    return this.titre?.touched && this.titre?.invalid
+  }
+
+  getControl(name: string) {
+    return getFormControl(this.form, name)
+  }
+
+  isControlInvalid(name: string) {
+    return isControlInvalid(this.getControl(name))
+  }
+
+  hasError(name: string, errorCode: string) {
+    return hasControlError(this.getControl(name), errorCode)
+  }
 }
